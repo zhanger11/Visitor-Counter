aafad9c0fa6b405e4496570cfe14f9b23801f634
Alex <zhangka@seas.upenn.edu>
2013-04-01 15:27:11 -0400

    final

--- index.php
+++ index.php
@@ -5,12 +5,4 @@ $result=exec('python counter.py');
 exec('python increment.py');
 exec('python time.py');
 echo $result;
-/*
-$count=("counter.txt");
-$hits=file($count);
-echo $hits[0];
-$hits[0] = $hits[0]+1;
-$fp=fopen($count,"w");
-fputs($fp,"$hits[0]");
-fclose($fp);*/
 ?>
